015ed5f1a52e0ae1df854c5cec58c0283a197021
sseifert <sseifert@pro-vision.de>
2019-03-05 00:03:11 +0100

    Use wcm.io Link Handler Link Reference Container and Granite UI widgets for link selection. Update test dependencies.

--- bundles/core/src/main/java/io/wcm/samples/core/config/impl/LinkHandlerConfigImpl.java
+++ bundles/core/src/main/java/io/wcm/samples/core/config/impl/LinkHandlerConfigImpl.java
@@ -19,11 +19,15 @@
  */
 package io.wcm.samples.core.config.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.osgi.service.component.annotations.Component;
 
 import com.day.cq.wcm.api.Page;
+import com.drew.lang.annotations.NotNull;
+import com.drew.lang.annotations.Nullable;
 
 import io.wcm.handler.link.spi.LinkHandlerConfig;
+import io.wcm.handler.link.type.MediaLinkType;
 import io.wcm.samples.core.config.AppTemplate;
 import io.wcm.wcm.commons.util.Template;
 
@@ -34,13 +38,21 @@ import io.wcm.wcm.commons.util.Template;
 public class LinkHandlerConfigImpl extends LinkHandlerConfig {
 
   @Override
-  public boolean isValidLinkTarget(Page page) {
+  public boolean isValidLinkTarget(@NotNull Page page) {
     return !Template.is(page, AppTemplate.ADMIN_STRUCTURE_ELEMENT);
   }
 
   @Override
-  public boolean isRedirect(Page page) {
+  public boolean isRedirect(@NotNull Page page) {
     return Template.is(page, AppTemplate.ADMIN_REDIRECT);
+  }
+
+  @Override
+  public @Nullable String getLinkRootPath(@NotNull Page page, @NotNull String linkTypeId) {
+    if (StringUtils.equals(linkTypeId, MediaLinkType.ID)) {
+      return "/content/dam/wcm-io-samples";
+    }
+    return super.getLinkRootPath(page, linkTypeId);
   }
 
 }
